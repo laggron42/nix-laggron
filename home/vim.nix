15059cebabe700e51fe398e68d63d4c56f4ec589
Auguste Charpentier <laggron42@gmail.com>
2024-01-15 21:26:07 +0100

    vim: remove vundle, use nixpkgs, move vimrc in repo

--- home/vim.nix
+++ home/vim.nix
@@ -8,10 +8,18 @@
   programs.vim = {
     enable = true;
     defaultEditor = true;
-    plugins = with pkgs.vimPlugins; [ Vundle-vim ];
-    extraConfig = builtins.readFile(builtins.fetchurl {
-      url = "https://gist.githubusercontent.com/laggron42/187570f694da4ae10eb3e5db6cf98205/raw/e95e997edf3580c210231698e63083e00b60b27a/vimrc";
-      sha256 = "03pckawnf5494hcwhz3ym00k1p1pryx5c0vj4s6gsw504gi4x227";
-    });
+    plugins = with pkgs.vimPlugins; [
+      vim-code-dark
+      vim-airline
+      coc-nvim
+      coc-sh
+      coc-clangd
+      coc-yaml
+      coc-json
+      coc-vimlsp
+      coc-pyright
+      DoxygenToolkit-vim
+    ];
+    extraConfig = builtins.readFile(dotfiles/vimrc);
   };
 }

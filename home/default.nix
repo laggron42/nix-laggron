0eb9c0855c0bd1a598d4511de6214bedecfe0d90
Auguste Charpentier <laggron42@gmail.com>
2024-02-28 18:50:02 +0100

    home: update to 23.11

--- home/default.nix
+++ home/default.nix
@@ -3,17 +3,21 @@
 {
   home.username = "laggron";
   home.homeDirectory = "/Users/laggron";
-  home.stateVersion = "23.05";
+  home.stateVersion = "23.11";
 
   programs.home-manager = {
     enable = true;
     path = "$HOME/nix-laggron";
   };
 
+  news.display = "silent";
+  systemd.user.startServices = "legacy";
+
   imports = [
     ./gnupg.nix
     ./git.nix
     ./vim.nix
     ./zsh.nix
+    ./neovim.nix
   ];
 }

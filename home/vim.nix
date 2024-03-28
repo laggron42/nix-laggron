79d2cc3057f0aa56a058a0a047d78bbd09649afd
Auguste Charpentier <laggron42@gmail.com>
2024-03-28 17:42:27 +0100

    vim: add tiger support

--- home/vim.nix
+++ home/vim.nix
@@ -1,5 +1,16 @@
 { config, pkgs, ... }:
 
+let
+  tiger-vim = pkgs.vimUtils.buildVimPlugin {
+    name = "tiger-vim";
+    src = pkgs.fetchFromGitHub {
+      owner = "BarrensZeppelin";
+      repo = "tiger.vim";
+      rev = "419d1823f117df3b184e4358feb3247a6bdc1f5a";
+      sha256 = "138r6fsgm5vy0v4882kvg2is0ajswm8qahhfzyhshnhgqa68yxas";
+    };
+  };
+in
 {
   home.packages = with pkgs; [
     nodejs
@@ -26,6 +37,7 @@
 
       # tools
       DoxygenToolkit-vim
+      tiger-vim
     ];
     extraConfig = builtins.readFile(dotfiles/vimrc);
   };
